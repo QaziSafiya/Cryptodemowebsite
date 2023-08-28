f3c740597f783accafcfc0d9992d931076464b74
safiya siddiqui <safiyaqazi517@gmail.com>
2023-08-28 21:06:56 +0530

    cryptotask

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Navbar';
-import Home from './Home';
-import About from './About';
-import Support from './Support';
-import Notifications from './Notifications';
-import Footer from './Footer';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Support from "./Support";
+import Notifications from "./Notifications";
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Home/>
-     <About/>
-     <Support/>
-     <Notifications/>
-     <Footer/>
+      <Navbar />
+      <Home />
+      <About />
+      <Support />
+      <Notifications />
+      <Footer />
     </div>
   );
 }
